3dd6c8ce3fd329d37f207f7a132902f86083dacf
danahoh <danahoh3@gmail.com>
2022-05-16 12:53:38 +0300

    edit MongoDB functions

--- src/main/java/DataAccess/MongoDB.java
+++ src/main/java/DataAccess/MongoDB.java
@@ -7,9 +7,41 @@ import java.net.UnknownHostException;
 
 
 public class MongoDB {
-    public static MongoClient mongoClient;
-    public static DB database;
-    public static DBCollection test;
+
+    private static final MongoDB instance = new MongoDB();
+
+    public static MongoDB getInstance(){
+        return instance;
+    }
+
+    private MongoDB() {
+        // users.add(new User("John", "john@domain.com"));
+        //users.add(new User("Susan", "susan@domain.com"));
+    }
+    DBConnector dbc = DBConnector.getInstance();
+    DB database = dbc.getConnection();
+
+    public DBCollection getDBCollection (String collectionName)
+    {
+        try{
+            DBCollection dbCollection = database.getCollection(collectionName);
+            return dbCollection;
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error getting the collection",e);
+        }
+    }
+
+    public DBCursor getObject(String collectionName, String key, String value)
+    {
+        DBCollection dbCollection = getDBCollection(collectionName);
+        DBObject q = new BasicDBObject(key,value);
+        DBCursor c = dbCollection.find(q);
+        return c;
+    }
+//    public static DB database;
+//    public static DBCollection test;
     public static void main(String[] args) throws UnknownHostException {
         mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         database = mongoClient.getDB("FootballSystem");
