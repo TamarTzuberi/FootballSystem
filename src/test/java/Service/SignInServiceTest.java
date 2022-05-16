f0239db6fe62967e900ac64172f48584d5ed742f
Stav <stavke@post.bgu.ac.il>
2022-05-16 20:47:50 +0300

    start tests

---
+++ src/test/java/Service/SignInServiceTest.java
@@ -0,0 +1,43 @@
+package Service;
+
+import Domain.DomainController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+
+public class SignInServiceTest {
+    private DomainController controller;
+
+    @Before
+    public void initialize() {
+        controller = DomainController.getDC();
+    }
+
+    public void testSignInRepresentative() {
+    }
+
+    public void testTestSignInRepresentative() {
+    }
+
+    public void testSignInGuest() {
+    }
+
+    public void testRegisterRole() {
+    }
+
+    @Test
+    @DisplayName("Test SignIn Guest")
+    public void testLogin() {
+        Assertions.assertTrue(controller.login("Stav","Stav123"));
+    }
+
+    public void testAddGame() {
+    }
+
+    public void testGamePlacement() {
+    }
+
+    public void testAddFinalScore() {
+    }
+}
