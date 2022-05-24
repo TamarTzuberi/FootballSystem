1e8fc91e06de1e2e57c7c4ada368c9e16312ad42
navitbranin <navitbranin123@gmail.com>
2022-05-24 10:47:32 +0300

    checkIfGameExist

--- src/main/java/DataAccess/GameDAO.java
+++ src/main/java/DataAccess/GameDAO.java
@@ -3,6 +3,7 @@ package DataAccess;
 import Domain.Game;
 import Domain.Subscriber;
 import Domain.User;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
@@ -10,6 +11,7 @@ import org.bson.types.ObjectId;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
 public class GameDAO implements DAO {
@@ -41,6 +43,29 @@ public class GameDAO implements DAO {
     // TODO Tamer
     public void update(Object game){
         // update exist game
+    }
+
+    public boolean checkIfGameExists(String key, String value)
+    {
+        try{
+            MongoCollection collection=database.getCollection("games");
+            List<Document> results = new ArrayList<>();
+            FindIterable<Document> iterable = collection.find();
+            iterable.into(results);
+            for(int i=0; i<results.size();i++)
+            {
+                Document d = results.get(i);
+                Document gameD =(Document)d.get("game");
+                String  valFromDoc = (String)gameD.get(key);
+                if(valFromDoc.equals(value))
+                    return true;
+            }
+            return false;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 
 }
