137a06d4e2cbce0ba9031e0099860a17babd6655
TamarTzuberi <tamar.tzuberi1@gmail.com>
2022-05-24 11:36:13 +0300

    update function in GameDAO

--- src/main/java/DataAccess/GameDAO.java
+++ src/main/java/DataAccess/GameDAO.java
@@ -3,15 +3,15 @@ package DataAccess;
 import Domain.Game;
 import Domain.Subscriber;
 import Domain.User;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
 public class GameDAO implements DAO {
@@ -40,32 +40,12 @@ public class GameDAO implements DAO {
         return d;
     }
 
-    // TODO Tamer
-    public void update(Object game){
-        // update exist game
-    }
 
-    public boolean checkIfGameExists(String key, String value)
-    {
-        try{
-            MongoCollection collection=database.getCollection("games");
-            List<Document> results = new ArrayList<>();
-            FindIterable<Document> iterable = collection.find();
-            iterable.into(results);
-            for(int i=0; i<results.size();i++)
-            {
-                Document d = results.get(i);
-                Document gameD =(Document)d.get("game");
-                String  valFromDoc = (String)gameD.get(key);
-                if(valFromDoc.equals(value))
-                    return true;
-            }
-            return false;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
+    public void update(String id,Object game){
+        MongoCollection collection=database.getCollection("games");
+        Bson globalFilter = Filters.eq("_id", id);
+        Document newGame = new Document("game",game);
+        collection.replaceOne(globalFilter,newGame);
     }
 
 }
